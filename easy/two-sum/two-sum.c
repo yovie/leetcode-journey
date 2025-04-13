8e11a47a9ba58f7a688881d1491b4b03f9700baf
yopi <yopi@tura.group>
2025-04-13 07:18:05 +0700

    two sum

---
+++ easy/two-sum/two-sum.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    int *result = malloc(sizeof(int) * 2);
+    *returnSize = 0;
+    int tmp;
+    for (int i=0; i<numsSize; i++) {
+        for (int j=i+1; j<numsSize; j++) {
+            result[0] = i;
+            tmp = target - nums[i];
+            if (tmp==nums[j]) {
+                result[1] = j;
+                *returnSize = 2;
+                return result;
+            }
+        }
+    }
+    free(result);
+    return NULL;
+}
+
+void main() {
+    int input[] = {2,7,11,15};
+    int *retSize = malloc(sizeof(int) * 1);
+    int *result = twoSum(input, 4, 9, retSize);
+    printf ("[%d, %d]", *(result), *(result+1));
+}
